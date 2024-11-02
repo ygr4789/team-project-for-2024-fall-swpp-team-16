ac89098442f824ce0b102572bb713ea41f2abf41
ygr4789 <yongdori@snu.ac.kr>
2024-11-02 15:12:19 +0900

    Feat: 플레이어 조작 테스트 씬 제작, 부드러운 이동 및 회전 구현

--- Assets/12.Tests/PlayerDev/Scripts/PlayerMovement.cs
+++ Assets/12.Tests/PlayerDev/Scripts/PlayerMovement.cs
@@ -6,20 +6,30 @@ namespace _12.Tests.PlayerDev
     {
         [Range(1f, 20f)]
         [SerializeField] private float _movementSpeed;
+        
         [Tooltip("run multiplier of the movement speed")]
         [Range(1f, 20f)]
         [SerializeField] private float _runMultiplier;
+        
         [SerializeField] private float _gravity = -9.81f;
-        [Range(1f, 20f)]
+        [Range(0f, 3f)]
         [SerializeField] private float _jumpHeight;
+        
+        [Tooltip("time taken to rotate when the direction of movement is changed")]
+        [Range(0f, 0.1f)]
+        [SerializeField] private float _smoothTime = 0.1f;
 
         private CharacterController characterController;
-        Vector3 _controllerVelocity;
+        private Transform playerTransform;
+        private Animator playerAnimator;
+        private Vector3 _controllerVelocity;
 
         // Start is called before the first frame update
         void Start()
         {
             characterController = GetComponent<CharacterController>();
+            playerTransform = transform.Find("Player");
+            playerAnimator = playerTransform.GetComponent<Animator>();
         }
 
         // Update is called once per frame
@@ -38,12 +48,23 @@ namespace _12.Tests.PlayerDev
             // moves the controller in the desired direction on the x- and z-axis
             Vector3 movement = transform.right * moveX + transform.forward * moveZ;
             characterController.Move(movement * (_movementSpeed * Time.deltaTime));
+            
 
+            // set player's forward same as moving direction
+            var currentVelocity = movement.magnitude;
+            if (currentVelocity > 0)
+            {
+                var targetAngle = Mathf.Atan2(moveX, moveZ) * Mathf.Rad2Deg - 90;
+                var angle = Mathf.SmoothDampAngle(playerTransform.eulerAngles.y, targetAngle, ref currentVelocity, _smoothTime);
+                playerTransform.rotation = Quaternion.Euler(0, angle, 0);
+            }
+                
             // gravity affects the controller on the y-axis
             _controllerVelocity.y += _gravity * Time.deltaTime;
 
             // moves the controller on the y-axis
             characterController.Move(_controllerVelocity * Time.deltaTime);
+            
 
             // the controller is able to jump when on the ground
             if (Input.GetButton("Jump") && characterController.isGrounded)
