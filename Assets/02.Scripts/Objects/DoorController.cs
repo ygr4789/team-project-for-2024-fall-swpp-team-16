ca71698ee5cc1c026e03ccd98e565ca7871334a3
kangraesan <sam2281202@gmail.com>
2024-11-13 05:54:48 +0900

    feat: playing answer notes and opening door when correct & door opening animation & add sfx for wrong answer & add sound effect pack asset

--- Assets/02.Scripts/Objects/DoorController.cs
+++ Assets/02.Scripts/Objects/DoorController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class DoorController : MonoBehaviour
 {
 	public GameObject scoreUIPanel; // Reference to the score UI panel
+    public int[] answerNotes; // Array to store notes for this door
+    private List<int> playedNotes = new List<int>(); // List to store played notes
+    public GameObject doorWing; // Reference to the door object
 
     void Start()
     {
@@ -27,9 +30,72 @@ public class DoorController : MonoBehaviour
         }
     }
 
+    void Update()
+    {
+        // if the score UI is active
+        if (scoreUIPanel.activeSelf)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1)) playedNotes.Add(1);
+            if (Input.GetKeyDown(KeyCode.Alpha2)) playedNotes.Add(2);
+            if (Input.GetKeyDown(KeyCode.Alpha3)) playedNotes.Add(3);
+            if (Input.GetKeyDown(KeyCode.Alpha4)) playedNotes.Add(4);
+            if (Input.GetKeyDown(KeyCode.Alpha5)) playedNotes.Add(5);
+            if (Input.GetKeyDown(KeyCode.Alpha6)) playedNotes.Add(6);
+            if (Input.GetKeyDown(KeyCode.Alpha7)) playedNotes.Add(7);
+            if (Input.GetKeyDown(KeyCode.Alpha8)) playedNotes.Add(8);
+
+            if (playedNotes.Count == answerNotes.Length)
+            {
+                if (CheckNotes())
+                {
+                    OpenDoor();
+                    playedNotes.Clear();
+                    scoreUIPanel.SetActive(false);
+                }
+                else
+                {
+                    Debug.Log("Incorrect notes played. Try again.");
+                    GameManager.sm.PlaySound("wrong-answer");
+                    playedNotes.Clear();
+                }
+            }
+        }
+    }
+
+    private bool CheckNotes()
+    {
+        for (int i = 0; i < answerNotes.Length; i++)
+        {
+            if (playedNotes[i] != answerNotes[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }        
+
 	private void OpenDoor()
     {
 		GameManager.sm.PlaySound("opening-door");
        	// opening door animation
+        StartCoroutine(OpenDoorAnimation(doorWing));
+    }
+    
+    private IEnumerator OpenDoorAnimation(GameObject door)
+    {
+        float duration = 1.5f; // animation duration
+        float startRotation = door.transform.eulerAngles.y;
+        float endRotation = startRotation + 90;
+        float time = 0;
+
+        while (time < duration)
+        {
+            float yRotation = Mathf.Lerp(startRotation, endRotation, time / duration);
+            door.transform.eulerAngles = new Vector3(door.transform.eulerAngles.x, yRotation, door.transform.eulerAngles.z);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        door.transform.eulerAngles = new Vector3(door.transform.eulerAngles.x, endRotation, door.transform.eulerAngles.z);
     }
 }
