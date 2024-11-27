0027b3bd5f40a918be053828568a4d5ec4d6861f
ygr4789 <yongdori@snu.ac.kr>
2024-11-27 22:29:01 +0900

    fix: 부유물끼리 충돌 시 밀림, 겹침 현상 방지

--- Assets/02.Scripts/Objects/FlotageController.cs
+++ Assets/02.Scripts/Objects/FlotageController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 public class FlotageController : MonoBehaviour
@@ -8,10 +9,11 @@ public class FlotageController : MonoBehaviour
     private const string PlayerTag = "Player"; // Tag of the player to follow
     
     [Range(0f, 10f)]
+    [Tooltip("movement speed at resonance")]
     public float moveSpeed = 3f;
     
     private GameObject _player; // Reference to the player object
-    private Rigidbody _rigidbody;
+    private Rigidbody _rigidbody; // Rigidbody component
     
     private void Awake()
     {
@@ -35,23 +37,25 @@ public class FlotageController : MonoBehaviour
         _player = GameObject.FindGameObjectWithTag(PlayerTag);
     }
 
-    // Update is called once per frame
     void Update()
     {
         FlattenPosition();
+        if (_rigidbody.velocity == Vector3.zero) _rigidbody.isKinematic = true;
     }
 
+    // Anchor to the water surface
     private void FlattenPosition()
     {
         Vector3 flattenPosition = transform.localPosition;
         flattenPosition.y = 0f;
         transform.localPosition = flattenPosition;
     }
-
+    
     private void MoveToPlayer()
     {
         if (_player is null) return;
-        var directionToPlayer = (_player.transform.position - transform.position).normalized;
+        _rigidbody.isKinematic = false;
+        var directionToPlayer = _player.transform.position - transform.position;
         directionToPlayer.y = 0;
         directionToPlayer.Normalize();
         _rigidbody.velocity = directionToPlayer * moveSpeed;
@@ -60,7 +64,8 @@ public class FlotageController : MonoBehaviour
     private void MoveAwayFromPlayer()
     {
         if (_player is null) return;
-        var directionAwayFromPlayer = (transform.position - _player.transform.position).normalized;
+        _rigidbody.isKinematic = false;
+        var directionAwayFromPlayer = transform.position - _player.transform.position;
         directionAwayFromPlayer.y = 0;
         directionAwayFromPlayer.Normalize();
         _rigidbody.velocity = directionAwayFromPlayer * moveSpeed;
