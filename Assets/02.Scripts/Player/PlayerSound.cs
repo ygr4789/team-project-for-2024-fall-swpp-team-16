390cd46e89ac619610a7e3c8f6b5546fa85cfd36
kangraesan <sam2281202@gmail.com>
2024-12-01 03:30:38 +0900

    feat: add step and jump landing sounds

--- Assets/02.Scripts/Player/PlayerSound.cs
+++ Assets/02.Scripts/Player/PlayerSound.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerSound : MonoBehaviour
@@ -11,9 +13,39 @@ public class PlayerSound : MonoBehaviour
     private void Awake()
     {
         playerAnimator = GetComponent<Animator>();
+        if (playerAnimator != null)
+        {
+            Debug.Log("Animator component found");
+        }
+        else
+        {
+            Debug.Log("Animator component not found");
+        }
+        
+        stepSounds = new string[]
+        {
+            "Footsteps_Walk_01",
+            "Footsteps_Walk_02",
+            "Footsteps_Walk_03",
+            "Footsteps_Walk_04",
+            "Footsteps_Walk_05",
+            "Footsteps_Walk_06",
+            "Footsteps_Walk_07",
+            "Footsteps_Walk_08",
+            "Footsteps_Walk_09",
+            "Footsteps_Walk_10",
+        };
     }
+
     public void PlayStepSound()
     {
-        GameManager.sm.PlayRandomSound(stepSounds);
+        Debug.Log("PlayStepSound");
+        GameManager.sm.PlayRandomSound(stepSounds, .4f);
+    }
+    
+    public void PlayLandSound()
+    {
+        Debug.Log("PlayLandSound");
+        GameManager.sm.PlaySound("Footsteps_Land_01", .3f);
     }
 }
