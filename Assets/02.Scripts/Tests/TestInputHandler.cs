777b97d881f490bc8cedc2ba3980bf2aca030543
ygr4789 <yongdori@snu.ac.kr>
2024-12-02 08:45:43 +0900

    test: SurfaceContact 테스트 씬, 스크립트

---
+++ Assets/02.Scripts/Tests/TestInputHandler.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestInputHandler : MonoBehaviour
+{
+    private PlayerInput input;
+    private SurfaceContactTest body;
+    // private SurfaceContactRigidbody body;
+
+    [Range(0f, 10f)]
+    [SerializeField] private float speed = 3f;
+    
+    private void Awake()
+    {
+        input = new PlayerInput();
+        body = GetComponent<SurfaceContactTest>();
+        // body = GetComponent<SurfaceContactRigidbody>();
+    }
+
+    private void Update()
+    {
+        GetInputs();
+        ApplyVelocity();
+    }
+    
+    private void GetInputs()
+    {
+        input.Clear();
+        if(!input.active) return;
+        
+        // get the movement input
+        input.moveX = Input.GetAxis("Horizontal");
+        input.moveZ = Input.GetAxis("Vertical");
+        input.jump = Input.GetButton("Jump");
+        input.run = Input.GetKey(KeyCode.LeftShift);
+    }
+
+    private void ApplyVelocity()
+    {
+        Vector3 velocity = transform.right * input.moveX + transform.forward * input.moveZ;
+        velocity *= speed; 
+        body.Velocity = velocity;
+        body.TriggerJump = input.jump;
+    }
+}
