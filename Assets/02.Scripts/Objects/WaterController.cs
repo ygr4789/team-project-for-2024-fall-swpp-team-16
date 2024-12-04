bb19aba1498dcbf3b1c1f0d4d163d069f1e3ccd8
ygr4789 <yongdori@snu.ac.kr>
2024-12-05 02:23:48 +0900

    fix: Handler에 WaterController 직접 전달 및 encapsulation, 부피 계산 오류 수정

--- Assets/02.Scripts/Objects/WaterController.cs
+++ Assets/02.Scripts/Objects/WaterController.cs
@@ -4,32 +4,43 @@ using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
 using UnityEngine.Assertions;
+using UnityEngine.Serialization;
 using Vector3 = UnityEngine.Vector3;
 
 [RequireComponent(typeof(BoxCollider))]
-public class WaterController : Interactable
+public class WaterController : MonoBehaviour
 {
-    [SerializeField, Range(0f, 10f)] private float minWaterLevel = 0f; // Minimum height of water
+    [SerializeField, Range(0f, 10f)] private float minWaterLevel = 0.1f; // Minimum height of water
     [SerializeField, Range(0f, 10f)] private float maxWaterLevel = 1f; // Maximum height of water
-    [Range(0f, 10f)] public float initialWaterLevel = 0.5f; // Original height of water
+    [SerializeField, Range(0f, 10f)] private float initialWaterLevel = 0.5f; // Original height of water
     
-    [SerializeField, Range(0f, 10f)] private float waterSurfaceSizeX = 1f; // Size of water surface
-    [SerializeField, Range(0f, 10f)] private float waterSurfaceSizeY = 1f; // Size of water surface
-    [SerializeField, Range(0f, 3f)] private float timeToReachTarget = 10f; // 목표에 도달할 시간
+    [SerializeField, Range(0f, 10f)] private float waterSurfaceSizeX = 1f; // X-Size of water surface
+    [SerializeField, Range(0f, 10f)] private float waterSurfaceSizeZ = 1f; // Z-Size of water surface
     [Tooltip("Water Surface Object (Must be child of this GameObject)")]
     [SerializeField] private Transform waterSurface;
+    [Tooltip("Ratio of exposed water surface area to the top surface area of the bounds.\n" +
+             "The smaller the value, the more sensitive the water level.")]
+    [SerializeField, Range(0.05f, 1f)] private float exposedSurfaceRatio = 0.2f;
+    
+    [SerializeField, Range(0f, 3f)] private float smoothTime = 0.5f; // Approximately the time it will take to reach the target water level
+    private Vector3 _currentVelocity = Vector3.zero;
+    
+    private BoxCollider _waterCollider;
+    private PlayerMovement _playerMovement;
+    private WaterSinkHandler _waterSinkHandler;
     
     [HideInInspector] public float currentWaterLevel;
-    [HideInInspector] public BoxCollider waterCollider;
+    
+    public Bounds Bounds => _waterCollider.bounds;
+    public float InitialWaterLevel => initialWaterLevel;
+    public float ExposedSurfaceRatio => exposedSurfaceRatio;
 
-    private PlayerMovement playerMovement;
-    // private WaterSinkHandler waterSinkHandler;
-
+    // Reflect changes to inspector values directly in the Editor Scene
     private void OnValidate()
     {
         Assert.IsNotNull(waterSurface, "Water Surface Object must be assigned");
         Assert.IsTrue(waterSurface.IsChildOf(transform), "Water Surface Object must be child of this GameObject");
-        Assert.IsNotNull(waterSurface.GetComponent<Renderer>(), @"Water Surface must have renderer component");
+        Assert.IsNotNull(waterSurface.GetComponent<Renderer>(), "Water Surface must have renderer component");
         minWaterLevel = Mathf.Clamp(minWaterLevel, 0f, maxWaterLevel);
         initialWaterLevel = Mathf.Clamp(initialWaterLevel, minWaterLevel, maxWaterLevel);
         Init();
@@ -42,8 +53,8 @@ public class WaterController : Interactable
     
     private void Init()
     {
-        waterCollider = gameObject.GetComponent<BoxCollider>();
-        // waterSinkHandler = new WaterSinkHandler(this);
+        _waterCollider = gameObject.GetComponent<BoxCollider>();
+        _waterSinkHandler = new WaterSinkHandler(this);
         currentWaterLevel = initialWaterLevel;
         SetWaterSurfacePosition();
         SetWaterSurfaceSize();
@@ -52,31 +63,47 @@ public class WaterController : Interactable
 
     private void Start()
     {
-        playerMovement = GameManager.gm.controller.GetComponent<PlayerMovement>();
+        _playerMovement = GameManager.gm.controller.GetComponent<PlayerMovement>();
     }
 
-    void Update()
+    private void Update()
     {
         CheckPlayerCollision();
-        SetWaterSurfacePosition();
-        // waterSinkHandler.Handle();
+        _waterSinkHandler.Handle();
+        SmoothApplyWaterSurfacePosition();
     }
 
+    // Check for collisions with player
+    // Respawn if they fall below a certain depth
     private void CheckPlayerCollision()
     {
+        const int maxDetectCounts = 10;
         var playerPosition = GameManager.gm.controller.position;
         var sinkHeight = playerPosition.y - waterSurface.position.y;
         var waterLayer = 1 << LayerMask.NameToLayer("Water");
-        Collider[] results = {};
-        var size = Physics.OverlapSphereNonAlloc(playerPosition, 3f, results, waterLayer);
-        if (Array.IndexOf(results, waterCollider) > -1 && sinkHeight < -0.5f)
+        var results = new Collider[maxDetectCounts];
+        Physics.OverlapSphereNonAlloc(playerPosition, 3f, results, waterLayer);
+        if (Array.IndexOf(results, _waterCollider) > -1 && sinkHeight < -0.5f)
         {
-            playerMovement.Drown();
+            _playerMovement.Drown();
         }
     }
 
+    private void SmoothApplyWaterSurfacePosition()
+    {
+        currentWaterLevel = Mathf.Clamp(currentWaterLevel, minWaterLevel, maxWaterLevel);
+        var waterSurfacePosition = Vector3.zero;
+        waterSurfacePosition.y = currentWaterLevel;
+        waterSurface.localPosition = Vector3.SmoothDamp(
+            waterSurface.localPosition,
+            waterSurfacePosition,
+            ref _currentVelocity,
+            smoothTime);
+    }
+    
     private void SetWaterSurfacePosition()
     {
+        currentWaterLevel = Mathf.Clamp(currentWaterLevel, minWaterLevel, maxWaterLevel);
         var waterSurfacePosition = Vector3.zero;
         waterSurfacePosition.y = currentWaterLevel;
         waterSurface.localPosition = waterSurfacePosition;
@@ -86,25 +113,42 @@ public class WaterController : Interactable
     {
         var waterSurfaceSize = Vector3.one;
         waterSurfaceSize.x = waterSurfaceSizeX;
-        waterSurfaceSize.y = waterSurfaceSizeY;
+        waterSurfaceSize.z = waterSurfaceSizeZ;
         waterSurface.localScale = waterSurfaceSize;
     }
     
     private void SetWaterCollider()
     {
         var bounds = waterSurface.GetComponent<Renderer>().bounds;
-        var center = Vector3.up * (maxWaterLevel + minWaterLevel) / 2;
-        var size = new Vector3(bounds.size.x, maxWaterLevel - minWaterLevel, bounds.size.z);
-        waterCollider.center = center;
-        waterCollider.size = size;
-        waterCollider.isTrigger = true;
+        var center = Vector3.up * maxWaterLevel / 2;
+        var size = new Vector3(bounds.size.x, maxWaterLevel, bounds.size.z);
+        _waterCollider.center = center;
+        _waterCollider.size = size;
+        _waterCollider.isTrigger = true;
     }
 
+    // Rock 오브젝트만 물에 빠졌을 때 상호작용함
+    // 임시로 사용, 리팩토링 시 필요할 경우 Interactable에서 Strategy 패턴 적용 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.TryGetComponent<RockController>(out _))
+            _waterSinkHandler.AddSinkingTarget(other.transform);
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.TryGetComponent<RockController>(out _))
+            _waterSinkHandler.RemoveSinkingTarget(other.transform);
+    }
+    
+    // Debugging, Visualize the range of movement of a water surface
     private void OnDrawGizmos()
     {
-        var boxCollider = GetComponent<BoxCollider>();
+        var bounds = waterSurface.GetComponent<Renderer>().bounds;
+        var center = Vector3.up * (maxWaterLevel + minWaterLevel) / 2;
+        var size = new Vector3(bounds.size.x, maxWaterLevel - minWaterLevel, bounds.size.z);
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(transform.position + boxCollider.center , boxCollider.size);
+        Gizmos.DrawWireCube(transform.position + center, size);
     }
 }
 
--- Assets/02.Scripts/Objects/WaterSinkHandler.cs
+++ Assets/02.Scripts/Objects/WaterSinkHandler.cs
@@ -5,68 +5,52 @@ using UnityEngine;
 
 public class WaterSinkHandler
 {
-    private WaterController waterController;
-    private readonly List<Collider> sunkColliders;
+    private readonly WaterController _waterController;
+    private readonly List<Collider> _sunkColliders;
 
     public WaterSinkHandler(WaterController waterController)
     {
-        this.waterController = waterController;
-        sunkColliders = new List<Collider>();
+        _waterController = waterController;
+        _sunkColliders = new List<Collider>();
     }
 
     public void AddSinkingTarget(Transform transform)
     {
-        if (!transform.TryGetComponent<RockController>(out _)) return;
         if (!transform.TryGetComponent<Collider>(out var collider)) return;
-        if (!sunkColliders.Contains(collider)) sunkColliders.Add(collider);
+        if (!_sunkColliders.Contains(collider)) _sunkColliders.Add(collider);
     }
 
     public void RemoveSinkingTarget(Transform transform)
     {
-        if (!transform.TryGetComponent<RockController>(out _)) return;
         if (!transform.TryGetComponent<Collider>(out var collider)) return;
-        sunkColliders.Remove(collider);
+        _sunkColliders.Remove(collider);
     }
 
     public void Handle()
     {
-        Debug.Log(sunkColliders.Count);
-        
-        waterController.currentWaterLevel = CalculateWaterHeight(
-            ConvertToBoundsArray(sunkColliders),
-            waterController.waterCollider.bounds,
-            waterController.initialWaterLevel,
-            waterController.currentWaterLevel);
-    }
-    
-    private static Bounds[] ConvertToBoundsArray(List<Collider> colliders)
-    {
-        return colliders
-            .ConvertAll(collider => collider.bounds)
-            .ToArray();
+        _waterController.currentWaterLevel = CalculateWaterLevel(_waterController.currentWaterLevel);
     }
 
-    private static float CalculateWaterHeight(Bounds[] sunkBoundsArray, Bounds waterBounds, float initialWaterHeight, float currentWaterHeight)
+    private float CalculateWaterLevel(float currentWaterLevel)
     {
+        var waterBounds = _waterController.Bounds;
+        
         // Calculate the cross-sectional area of the cup
-        var waterArea = (waterBounds.max.x - waterBounds.min.x) * (waterBounds.max.y - waterBounds.min.y);
+        var waterArea = (waterBounds.max.x - waterBounds.min.x) * (waterBounds.max.z - waterBounds.min.z) * _waterController.ExposedSurfaceRatio;
 
         // Calculate the displaced volume
         var sunkVolume = (
-            from sunkBounds in sunkBoundsArray
-            let submergedMinZ = Mathf.Max(sunkBounds.min.z, waterBounds.min.z)
-            let submergedMaxZ = Mathf.Min(sunkBounds.max.z, currentWaterHeight)
-            where submergedMaxZ > submergedMinZ 
-            select (sunkBounds.max.x - sunkBounds.min.x) * (sunkBounds.max.y - sunkBounds.min.y) * (submergedMaxZ - submergedMinZ)
+            from sunkCollider in _sunkColliders
+            let sunkBounds = sunkCollider.bounds
+            let submergedMinY = Mathf.Max(sunkBounds.min.y, waterBounds.min.y)
+            let submergedMaxY = Mathf.Min(sunkBounds.max.y, waterBounds.min.y + currentWaterLevel)
+            where submergedMaxY > submergedMinY 
+            select (sunkBounds.max.x - sunkBounds.min.x) * (sunkBounds.max.z - sunkBounds.min.z) * (submergedMaxY - submergedMinY)
             ).Sum();
 
         // Calculate the change in water height
         var deltaHeight = sunkVolume / waterArea;
-        var updatedWaterHeight = initialWaterHeight + deltaHeight;
-
-        // Clamp water height to the water bounds
-        updatedWaterHeight = Mathf.Min(updatedWaterHeight, waterBounds.max.z);
-
-        return updatedWaterHeight;
+        var updatedWaterLevel = _waterController.InitialWaterLevel + deltaHeight;
+        return updatedWaterLevel;
     }
 }
