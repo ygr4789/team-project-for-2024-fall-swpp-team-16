05c9fb8a1aa5b4dec63a6b20f47de56f7d541e15
silee1103 <silee1103@snu.ac.kr>
2024-12-14 14:04:22 +0900

    feat: make billboard script

--- Assets/02.Scripts/Objects/FloatingTextController.cs
+++ Assets/02.Scripts/Objects/FloatingTextController.cs
@@ -9,19 +9,12 @@ public class FloatingTextController : MonoBehaviour
     public Vector3 offset = new Vector3(0, 3, 0);
     public string inspectGuideText = "Press [E]";
     public float inspectMaxDistance = 3;
-    private Camera mainCamera;
 
     void Start()
     {
         if (target == null)
         {
             Debug.LogError("FloatingTextController: target object is not set!");
-        }
-
-        mainCamera = Camera.main; // 메인 카메라 참조
-        if (mainCamera == null)
-        {
-            Debug.LogError("FloatingTextController: Main Camera not found!");
         }
 
         // 텍스트 설정
@@ -44,13 +37,6 @@ public class FloatingTextController : MonoBehaviour
         if (target != null)
         {
             transform.position = target.transform.position + offset;
-        }
-
-        // 항상 카메라를 향하도록 회전
-        if (mainCamera != null)
-        {
-            Vector3 cameraForward = mainCamera.transform.forward;
-            transform.rotation = Quaternion.LookRotation(cameraForward, Vector3.up);
         }
 
         // if E is pressed, trigger the inspect function of the target object
